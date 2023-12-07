d3e0a2aaa3b39946c6c6e6e25116b031cf5ead09
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-07 21:05:00 +0300

    feat: add productUpdateValidation and CommissionUpdateValidation + comments

--- validations.js
+++ validations.js
@@ -1,5 +1,6 @@
 import { body } from 'express-validator';
 
+//auth valid
 export const loginValidation = [
     body('email', 'Invalid email format').isEmail(),
     body('password', 'Password shoud be at least 8 symbols').isLength({ min: 8 }), 
@@ -12,6 +13,7 @@ export const registerValidation = [
     body('avatarUrl', 'Invalid url').optional().isURL(),
 ];
 
+//priducts valid
 export const productCreateValidation = [
     body('name', 'Enter product\'s name').isLength({ min: 3 }).isString(),
     body('description', 'Enter product\'s descriptions').isLength({ min: 10 }).isString(),
@@ -19,6 +21,14 @@ export const productCreateValidation = [
     body('imageUrl', 'Invalid url').optional().isURL(),
 ];
 
+export const productUpdateValidation = [
+    body('name', 'Enter product\'s name').optional().isLength({ min: 3 }).isString(),
+    body('description', 'Enter product\'s descriptions').optional().isLength({ min: 10 }).isString(),
+    body('features', 'Enter product\'s features').optional().isArray(), 
+    body('imageUrl', 'Invalid url').optional().isURL(),
+];
+
+//bathes valid
 export const batchCreateValidation = [
     body('name', 'Enter batch name').isLength({ min: 3 }).isString(),
     body('manufactureDate', 'Enter valid manufacture date').isISO8601().toDate(),
@@ -41,8 +51,15 @@ export const batchUpdateValidation = [
     body('defectedProducts.*', 'Invalid product ID').optional().isMongoId(), 
 ];
 
+//commission valid
 export const CommissionCreateValidation = [
     body('name', 'Enter comission person name').isLength({ min: 2 }).isString(),
     body('jobTitle', 'Enter comission person job title').isLength({ min: 2 }).isString(), 
+    body('imageUrl', 'Invalid url').optional().isURL(),
+];
+
+export const CommissionUpdateValidation = [
+    body('name', 'Enter comission person name').optional().isLength({ min: 2 }).isString(),
+    body('jobTitle', 'Enter comission person job title').optional().isLength({ min: 2 }).isString(), 
     body('imageUrl', 'Invalid url').optional().isURL(),
 ];
