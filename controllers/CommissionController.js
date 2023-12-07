32a08993dc74186fc539a1bfe515996e32c07ae8
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-07 21:04:21 +0300

    feat: add get, getone, remove, update for Commission controller

--- controllers/CommissionController.js
+++ controllers/CommissionController.js
@@ -18,3 +18,84 @@ export const create = async (req, res) => {
         });
     }
 };
+
+export const remove = async (req, res) => {
+    try {
+        const commissionId = req.params.id; 
+        
+        const doc = await CommissionModel.findByIdAndDelete(commissionId);
+
+        if (!doc) {
+            return res.status(404).json({
+                message: 'Commission member doesn\'t exist',
+            });
+        }
+
+        res.json({
+            success: true,
+        });
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Remove attempt failed',
+        });
+    }
+};
+
+export const getOne = async (req, res) => {
+    try{
+        const commissionId = req.params.id;
+
+        const doc = await CommissionModel.findById(commissionId);
+
+        if (doc) {
+            res.json(doc);
+        } else {
+            res.status(404).json({ message: 'Commission member not found' });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Search attempt failed',
+        });
+    }
+};
+
+export const getAll = async (req, res) => {
+    try{
+        const commissionMembers = await CommissionModel.find();
+
+        res.json(commissionMembers);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Search attempt failed',
+        });
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const commissionId = req.params.id;
+
+        await CommissionModel.updateOne(
+            {
+                _id: commissionId,
+            }, 
+            {
+                name: req.body.name,
+                jobTitle: req.body.jobTitle,
+                imageUrl: req.body.imageUrl, 
+            },
+        );
+
+        res.json({
+            success: true,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Update attempt failed',
+        });
+    }
+};
