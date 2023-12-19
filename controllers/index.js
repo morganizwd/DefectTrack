9c6777a5794025ff3fb5c76ec4aad1ea2495b1e9
Ilya Lobanovskiy <morganizwd@gmail.com>
2023-12-19 14:00:01 +0300

    dfdsfsdf

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,3 @@
-import Comission from '../models/Commission.js';
-
 export * as UserController from './UserController.js';
 export * as ProductsController from './ProductsController.js'; 
 export * as BatchesController from './BatchesController.js';
